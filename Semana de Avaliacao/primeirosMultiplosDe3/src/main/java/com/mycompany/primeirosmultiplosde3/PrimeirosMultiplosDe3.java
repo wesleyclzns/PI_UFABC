27e825144b70038888b5bcc9ae2434ac94050172
wesleyclzns <calazanscafe@outlook.com>
2022-10-15 16:30:59 -0300

    Primeiros Multiplos de 3 estão funcionando

--- Semana de Avaliacao/primeirosMultiplosDe3/src/main/java/com/mycompany/primeirosmultiplosde3/PrimeirosMultiplosDe3.java
+++ Semana de Avaliacao/primeirosMultiplosDe3/src/main/java/com/mycompany/primeirosmultiplosde3/PrimeirosMultiplosDe3.java
@@ -5,13 +5,18 @@ public class PrimeirosMultiplosDe3 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int numero;
-        int i =1;
+        int numeroCiclo;
+        int multi3 =3;
+        int i = 1; 
     
-
-        System.out.println("Digite um nuemro: ");
-        numero = entrada.nextInt();
+        System.out.println("Quantos multiplos de 3 voce quer?: ");
+        numeroCiclo = entrada.nextInt();
         
+       while (i <= numeroCiclo) {
+        System.out.print(multi3 + " ");
+        multi3 = multi3 + 3;
+        i++;
+       }
         
         
     }
