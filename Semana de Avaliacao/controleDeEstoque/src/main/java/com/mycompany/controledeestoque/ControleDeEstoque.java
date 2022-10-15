1ae86b726799e65fb95406a4fc90091beca62f7a
wesleyclzns <calazanscafe@outlook.com>
2022-10-15 19:19:43 -0300

    Controle de estoque esta parcialmente funcionando, mas precisa melhorar, esta dando nota 6

--- Semana de Avaliacao/controleDeEstoque/src/main/java/com/mycompany/controledeestoque/ControleDeEstoque.java
+++ Semana de Avaliacao/controleDeEstoque/src/main/java/com/mycompany/controledeestoque/ControleDeEstoque.java
@@ -1,17 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.controledeestoque;
 
-/**
- *
- * @author clzns
- */
+import java.util.Scanner;
 public class ControleDeEstoque {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        int quantEstoque, quantPedidos, quantItens;
+        int pedidosAtendidos = 0;
+        
+        System.out.println("Digite a quantidade em estoque");
+        quantEstoque = entrada.nextInt();
+
+        System.out.println("Digite o numero de pedidos");
+        quantPedidos = entrada.nextInt();
+
+        for (int i = 0; i < quantPedidos; i++) {
+            System.out.println("Digite a quantidade de unidades Solicitadas por pedido");
+            quantItens = entrada.nextInt();
+            
+            if (quantEstoque - quantItens < quantEstoque  && quantEstoque > 0) {
+                quantEstoque = quantEstoque - quantItens;
+                pedidosAtendidos++;
+            }
+
+            //System.out.println(quantEstoque);
+        }
+
+
+        
+
+        System.out.println(pedidosAtendidos);
     }
 }
