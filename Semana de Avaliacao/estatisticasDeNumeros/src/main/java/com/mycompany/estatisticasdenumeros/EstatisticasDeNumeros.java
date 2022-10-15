07520a8728a290b0dd155281b25b025bd30b78a6
wesleyclzns <calazanscafe@outlook.com>
2022-10-15 15:33:51 -0300

    Estatisticas de Numeros estão certinhos

--- Semana de Avaliacao/estatisticasDeNumeros/src/main/java/com/mycompany/estatisticasdenumeros/EstatisticasDeNumeros.java
+++ Semana de Avaliacao/estatisticasDeNumeros/src/main/java/com/mycompany/estatisticasdenumeros/EstatisticasDeNumeros.java
@@ -5,26 +5,41 @@ public class EstatisticasDeNumeros {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int numEntrada;
-        int soma = 0;
+        int primNumEntrada, numEntrada;
+        int soma;
         int minimo;
         int maximo;
         int i = 1;
         double media = 0;
 
+        System.out.println("Digite o numero vezes:");
+        primNumEntrada = entrada.nextInt();
+
         System.out.println("Digite um numero:");
-        numEntrada = entrada.nextInt();
-        while (i<=numEntrada) {
-            soma = soma + i;
+            numEntrada = entrada.nextInt();
+            minimo = soma = maximo = numEntrada;
+            
+        while (i < primNumEntrada) {
+            System.out.println("Digite + um numero:");
+            numEntrada = entrada.nextInt();
+            
+            soma = soma + numEntrada;
+
+            if (numEntrada > maximo) {
+                maximo = numEntrada;
+            }
+            if (numEntrada < minimo) {
+                minimo = numEntrada;
+            }
             i++;
-        }
-        minimo = 0;
-        maximo = numEntrada;
-        media = (double)soma /(double)numEntrada;
-        System.out.println(String.format("%.2f", media));
+        }  
+             
+        media = (double)soma /(double)(i);
         System.out.println(soma);
+        System.out.println(String.format("%.2f", media));
         System.out.println(minimo);
         System.out.println(maximo);
         entrada.close();
     }
 }
+
