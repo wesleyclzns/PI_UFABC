30d42eec3e63b6c601d7242c84b92581ed0253dc
wesleyclzns <calazanscafe@outlook.com>
2022-10-15 14:08:00 -0300

    Conta Multiplos de 3 e 5 finalizados

--- Pratica 1/MediaTresNotas/src/main/java/com/mycompany/mediatresnotas/MediaTresNotas.java
+++ Pratica 1/MediaTresNotas/src/main/java/com/mycompany/mediatresnotas/MediaTresNotas.java
@@ -7,7 +7,6 @@ public class MediaTresNotas {
     public static void main(String[] args) {
         double nota1, nota2, nota3, media;
         Scanner entrada = new Scanner(System.in);
-        
         System.out.println("Digite uma nota:");
         nota1 = entrada.nextDouble();
         System.out.println("Digite a proxima nota:");
--- Semana de Avaliacao/contaMultiplos/src/main/java/com/mycompany/contamultiplos/ContaMultiplos.java
+++ Semana de Avaliacao/contaMultiplos/src/main/java/com/mycompany/contamultiplos/ContaMultiplos.java
@@ -1,17 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.contamultiplos;
 
-/**
- *
- * @author clzns
- */
+import java.util.Scanner;
+
+import javax.swing.text.Style;
 public class ContaMultiplos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        int numUsuario;
+        int multpl3 = 0;
+        int multpl5 = 0;
+
+        System.out.println("Digite um numero:");
+        numUsuario = entrada.nextInt();
+        
+        while (numUsuario != 0) {
+            
+            if (numUsuario % 3 == 0) {
+                multpl3 = multpl3 + 1;
+            } else {
+                if (numUsuario % 5 == 0) {
+                    multpl5 = multpl5 + 1;
+                } else {
+                    
+                }  
+            }
+
+            System.out.println("Digite + um numero:");
+            numUsuario = entrada.nextInt();
+
+       }
+       System.out.println("Os multiplos de 3 foram");
+       System.out.println(multpl3);
+       System.out.println("Os multiplos de 5 foram");
+       System.out.println(multpl5);
+       entrada.close();
+
     }
 }
