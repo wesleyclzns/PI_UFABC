954c5dd927df670ab4ba7661577920eb27444c36
wesleyclzns <calazanscafe@outlook.com>
2022-10-16 18:05:21 -0300

    Desempenho preditivo 100%

--- Pratica 1/desempenhoPreditivo/src/main/java/com/mycompany/desempenhopreditivo/DesempenhoPreditivo.java
+++ Pratica 1/desempenhoPreditivo/src/main/java/com/mycompany/desempenhopreditivo/DesempenhoPreditivo.java
@@ -1,17 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.desempenhopreditivo;
 
-/**
- *
- * @author clzns
- */
+import java.util.Scanner;
 public class DesempenhoPreditivo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        int vp, fn, fp, vn; 
+        double acuracia, precisao, sensibilidade;
+
+        System.out.println("Digite o VP");
+        vp = entrada.nextInt();
+        System.out.println("Digite o FN");
+        fn = entrada.nextInt();
+        System.out.println("Digite o FP");
+        fp = entrada.nextInt();
+        System.out.println("Digite o VN");
+        vn = entrada.nextInt();
+
+
+        acuracia = ((double)vp + (double)vn) / ((double)vp + (double)vn + (double)fp + (double)fn);
+        precisao = (double)vp / ((double)vp + (double)fp);
+        sensibilidade = (double)vp / ((double)vp + (double)fn);
+
+        System.out.println(String.format("%.2f", acuracia));
+        System.out.println(String.format("%.2f", precisao));
+        System.out.println(String.format("%.2f", sensibilidade));
     }
 }
