d9e021ba784d004fa7b08835e23414dc1f5ef187
wesleyclzns <calazanscafe@outlook.com>
2022-10-16 11:42:05 -0300

    Controle de estoque funcionando 100%

--- Semana de Avaliacao/controleDeEstoque/src/main/java/com/mycompany/controledeestoque/ControleDeEstoque.java
+++ Semana de Avaliacao/controleDeEstoque/src/main/java/com/mycompany/controledeestoque/ControleDeEstoque.java
@@ -5,30 +5,25 @@ public class ControleDeEstoque {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int quantEstoque, quantPedidos, quantItens;
-        int pedidosAtendidos = 0;
+        int estoque, numeroDePedidos, quantidadeDeItens, pedidosAtendidos = 0;
         
-        System.out.println("Digite a quantidade em estoque");
-        quantEstoque = entrada.nextInt();
+       System.out.println("Digite a quantidade em estoque:");
+        estoque = entrada.nextInt();
+        System.out.println("Digite o numero de pedidos:");
+        numeroDePedidos = entrada.nextInt();
 
-        System.out.println("Digite o numero de pedidos");
-        quantPedidos = entrada.nextInt();
+        for (int i = 0; i < numeroDePedidos; i++) {
+            System.out.println("Digite a quantidade de itens pedidos:");
+            quantidadeDeItens = entrada.nextInt();
 
-        for (int i = 0; i < quantPedidos; i++) {
-            System.out.println("Digite a quantidade de unidades Solicitadas por pedido");
-            quantItens = entrada.nextInt();
-            
-            if (quantEstoque - quantItens < quantEstoque  && quantEstoque > 0) {
-                quantEstoque = quantEstoque - quantItens;
-                pedidosAtendidos++;
+            if (quantidadeDeItens <= estoque) {
+                estoque -= quantidadeDeItens;
+                pedidosAtendidos += 1;
             }
-
-            //System.out.println(quantEstoque);
+            
         }
-
-
-        
-
         System.out.println(pedidosAtendidos);
+        entrada.close();
     }
 }
+ 
