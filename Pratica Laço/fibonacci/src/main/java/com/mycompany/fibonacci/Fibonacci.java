d46ac7d8478306f4adbf8fbc8c1a12f44f3dce8b
wesleyclzns <calazanscafe@outlook.com>
2022-10-18 15:23:42 -0300

    Fibonaci Funciona

--- Pratica Laço/fibonacci/src/main/java/com/mycompany/fibonacci/Fibonacci.java
+++ Pratica Laço/fibonacci/src/main/java/com/mycompany/fibonacci/Fibonacci.java
@@ -1,17 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.fibonacci;
 
-/**
- *
- * @author clzns
- */
+import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        int corrente = 0, ultimo = 1, penultimo = 1, ciclo;
+
+        System.out.println("Digite quantos numeros fibonaci vc quer:");
+        ciclo = entrada.nextInt();
+
+        for (int i = 0; i < ciclo; i++){
+        corrente = ultimo + penultimo;
+        penultimo = ultimo;
+        ultimo = corrente;
+        }
+        System.out.println(corrente);
     }
+    
 }
