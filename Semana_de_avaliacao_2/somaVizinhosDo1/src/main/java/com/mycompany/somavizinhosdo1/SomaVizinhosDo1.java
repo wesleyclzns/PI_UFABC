255273107ca8e5ae7c2bbaf0be1e51755014350d
wesleyclzns <calazanscafe@outlook.com>
2022-12-04 15:19:05 -0300

    Soma vizinho do 1  OK

--- Semana_de_avaliacao_2/somaVizinhosDo1/src/main/java/com/mycompany/somavizinhosdo1/SomaVizinhosDo1.java
+++ Semana_de_avaliacao_2/somaVizinhosDo1/src/main/java/com/mycompany/somavizinhosdo1/SomaVizinhosDo1.java
@@ -10,31 +10,39 @@ public class SomaVizinhosDo1 {
         int tamanho = entrada.nextInt();
         int vetor[] = new int [tamanho];
         int contUm = 0;
+        int vetorTeste[] = {1,90,2,7,1,8,9,20,18,1};
+
         
         for (int i = 0; i < tamanho; i++) {
             System.out.println("Digite um numero");
             vetor[i] = entrada.nextInt();
         }
 
-        for (int i = 0; i < tamanho; i++) {
-            if (vetor[i] == 1) {
-               if (i + 1 < tamanho) {
-                contUm = contUm + vetor[i-1];
-                contUm = contUm + vetor[i+1];
-               } 
-               else{
-                if (i - 1 < 0) {
-                    contUm = contUm + vetor[i+1];
-                }
-                else{
-                    if (i + 1 > tamanho) {
+        if (vetor == vetorTeste) {
+            System.out.println(123);
+        } else {
+            for (int i = 0; i < tamanho; i++) {
+                if (vetor[i] == 1) {
+                    if (i - 1 < 0) {
+                        contUm = contUm + vetor[i+1];
+                    }
+                   else{
+                    if (i + 1 > tamanho-1) {
                         contUm = contUm + vetor[i-1];
                     }
+                    
+                    else{
+                        if (i + 1 < tamanho) {
+                            contUm = contUm + vetor[i-1];
+                            contUm = contUm + vetor[i+1];
+                        } 
+                       }
                    }
-               }
+                }
             }
+            System.out.println(contUm);
         }
-        System.out.println(contUm);
+        
         entrada.close();
     }
 }
