a455f9b3f2a0f0008ab25b44e49aba739af8b3ac
wesleyclzns <calazanscafe@outlook.com>
2022-12-04 16:52:28 -0300

    Compar vetores OK

--- Semana_de_avaliacao_2/comparecaoDeVetores/src/main/java/com/mycompany/comparecaodevetores/ComparecaoDeVetores.java
+++ Semana_de_avaliacao_2/comparecaoDeVetores/src/main/java/com/mycompany/comparecaodevetores/ComparecaoDeVetores.java
@@ -1,17 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.comparecaodevetores;
 
-/**
- *
- * @author clzns
- */
+import java.util.Scanner;
 public class ComparecaoDeVetores {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a quantidade de alunos");
+        int quantNumeros = entrada.nextInt();
+        int vetorA[] = new int [quantNumeros];
+        int vetorB[] = new int [quantNumeros];
+        int compativel = 0;
+        
+
+        for (int i = 0; i < quantNumeros; i++) {
+            System.out.println("Digite os valores do vetor A");
+            vetorA[i] = entrada.nextInt();
+        }
+        for (int i = 0; i < quantNumeros; i++) {
+            System.out.println("Digite os valores do vetor B");
+            vetorB[i] = entrada.nextInt();
+        }
+
+        int multip = vetorB[1]/vetorA[1];
+        for (int i = 0; i < quantNumeros; i++) {
+            if (multip == vetorB[i]/vetorA[i]) {
+                compativel = compativel + 0;
+            } else {
+                compativel = compativel + 1;
+            }
+        }
+        if (compativel == 0) {
+            System.out.println("SIM");
+        }else{
+            System.out.println("NAO");
+        }
+
+        entrada.close();
     }
 }
