ae7e8db766e6e77f067541fd8ed3fdc382035547
wesleyclzns <calazanscafe@outlook.com>
2022-12-05 15:58:06 -0300

    Compar matrizes 5.75

--- semana_de_avaliacao_3/MultiplicacaoDeMatrizes/src/main/java/com/mycompany/multiplicacaodematrizes/MultiplicacaoDeMatrizes.java
+++ semana_de_avaliacao_3/MultiplicacaoDeMatrizes/src/main/java/com/mycompany/multiplicacaodematrizes/MultiplicacaoDeMatrizes.java
@@ -4,6 +4,70 @@ import java.util.Scanner;
 public class MultiplicacaoDeMatrizes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de numeros de linhas ");
+        int linhas1 = entrada.nextInt();
+        System.out.println("Digite a quantidade de numeros de colunas ");
+        int colunas1 = entrada.nextInt();
+    
+        int matrizA[][] = new int [linhas1][colunas1];
+        
+        for (int l = 0; l < linhas1; l++) {
+            for (int c = 0; c < colunas1; c++) {
+                System.out.println("Digite um Numero: ");
+                matrizA[l][c] = entrada.nextInt();
+            }
+        }
+
+        System.out.println("Digite a quantidade de numeros de linhas ");
+        int linhas2 = entrada.nextInt();
+        System.out.println("Digite a quantidade de numeros de colunas ");
+        int colunas2 = entrada.nextInt();
+    
+        int matrizB[][] = new int [linhas2][colunas2];
+        
+        for (int l = 0; l < linhas2; l++) {
+            for (int c = 0; c < colunas2; c++) {
+                System.out.println("Digite um Numero: ");
+                matrizB[l][c] = entrada.nextInt();
+            }
+        }
+
+        int linhas = 0;
+        int colunas = 0;
+        if (linhas1 < linhas2) {
+            linhas = linhas2;
+        } else {
+            if (linhas1 > linhas2) {
+                linhas = linhas1;
+            }
+            
+        }
+        if (colunas1 < colunas2) {
+            colunas = colunas2;
+        } else {
+            if (colunas1 > colunas2) {
+                colunas = colunas1;
+            }
+            
+        }
+        int matrizAB[][] = new int [linhas][colunas];
+
+        for (int l = 0; l < linhas; l++) {
+            for (int c = 0; c < colunas; c++) {
+                matrizAB[l][c] = (matrizA[l][c] * matrizB[l][c]) * 3;
+            }
+        }
+
+        for (int l = 0; l < linhas; l++) {
+            for (int c = 0; c < colunas; c++) {
+                System.out.println(matrizAB[l][c]+" ");
+                System.out.print("");
+            }
+        }
+
+
+        entrada.close();
     }
 }
--- semana_de_avalicacao_4/comparaMatrizes/src/main/java/com/mycompany/comparamatrizes/ComparaMatrizes.java
+++ semana_de_avalicacao_4/comparaMatrizes/src/main/java/com/mycompany/comparamatrizes/ComparaMatrizes.java
@@ -5,10 +5,30 @@ public class ComparaMatrizes {
 
     public static void main(String[] args) {
         public static boolean compararMatrizes(int[][] matriz1, int[][] matriz2) {
-            boolean matriz;
-            matriz2[1][1] == 2*matriz1[1][1]
+            boolean matriz = false;
+            
+            if (matriz1.length == matriz2.length) {
+                for (int i = 0; i < matriz1.length; i++) {
+                    for (int j = 0; j < matriz1.length; j++) {
+                        if (matriz2[i][j] == matriz1[i][j]*2) {
+                            matriz = true;
+                        } else{
+                            if (matriz2[i][j] == matriz1[i][j]) {
+                                matriz = false;
+                            } else {
+                                matriz = false;
+                            }
+                        }
+                    }
+                }
+            } else {
+                matriz = false;
+            }
+            
 
             return matriz;
         }
     }
 }
+
+
